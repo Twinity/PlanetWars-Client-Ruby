498b830359261e0b66fccf7bb221bafb7de1fc3c
Borderliner <hajianpour.mr@gmail.com>
2016-08-02 21:53:11 +0430

    Added Req class to send Get request

---
+++ src/Req.rb
@@ -0,0 +1,45 @@
+require 'net/http'
+require 'uri'
+
+class Req
+
+  def initialize(url)
+    get url
+  end
+
+  def get(url)
+    uri = URI.parse(url)
+    @http = Net::HTTP.new(url.host, uri.port)
+    @request = Net::HTTP::Get.new(uri.to_s)
+    return self
+  end
+
+  def body
+    return @body
+  end
+
+  def headers(headers)
+    headers.each do |key, val|
+      @request[key] = val
+    end
+    return self
+  end
+
+  def asJson
+    @request["Accept"] = "application/json"
+    return self
+  end
+
+  def body
+    return @response.body
+  end
+
+  def status_code
+    return @response.code
+  end
+
+  def exec
+    @response = http.request request
+    return self
+  end
+end
