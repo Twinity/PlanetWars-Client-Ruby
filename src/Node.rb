42dca44ce5ab23c0760156bcb5bf8acc96b85c3d
Borderliner <hajianpour.mr@gmail.com>
2016-08-05 00:24:13 +0430

    Created Node class, but I wonder if it's really necessary.

--- src/Node.rb
+++ src/Node.rb
@@ -1,2 +1,26 @@
 class Node
+
+  def initialize
+    @id = 0
+    @army_count = 0
+    @owner = 0
+    @adjacents = Array.new
+  end
+
+  def get_id
+    @id
+  end
+
+  def get_army_count
+    @army_count
+  end
+
+  def get_owner
+    @owner
+  end
+
+  def get_adjacents
+    @adjacents
+  end
+
 end
